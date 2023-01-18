42ddf8f11a03596188982c446f40a271e863dfb4
katiesusannacollins <k.collins@nhm.ac.uk>
2023-01-18 12:14:30 +0000

    changed data generation

--- script.R
+++ script.R
@@ -4,7 +4,7 @@ library(tidyverse)
 
 dat <- tibble(X=rnorm(150, mean=20, sd=10), 
               Y=rnorm(150, mean=65, sd=20),
-              sp=paste0("Sp1"))
+              sp=rep(c("sp1", "sp2", "sp3"), each=50))
 
 ggplot(dat, aes(X, Y, col=sp))+
   geom_point()
