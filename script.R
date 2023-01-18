9c1a8ee7068a3ef4090ca2b932561bf657795c72
katiesusannacollins <k.collins@nhm.ac.uk>
2023-01-18 12:10:45 +0000

    changed data read to data generation, added plot code

--- script.R
+++ script.R
@@ -2,4 +2,9 @@
 
 library(tidyverse)
 
-dat <- read.csv(file=file.choose())
+dat <- tibble(X=rnorm(150, mean=20, sd=10), 
+              Y=rnorm(150, mean=65, sd=20),
+              sp=paste0("Sp1"))
+
+ggplot(dat, aes(X, Y, col=sp))+
+  geom_point()
